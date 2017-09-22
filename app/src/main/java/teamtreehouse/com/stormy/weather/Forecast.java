151409918c7ee21ecdbb33086a7fba4a95eebec4
Chris Gaona <thebeloved88@sbcglobal.net>
2017-09-22 08:47:38 -0700

    Creates required data models; refactors to make code more modular & clear

---
+++ app/src/main/java/teamtreehouse/com/stormy/weather/Forecast.java
@@ -0,0 +1,11 @@
+package teamtreehouse.com.stormy.weather;
+
+/**
+ * Created by chrisgaona on 9/22/17.
+ */
+
+public class Forecast {
+    private Current mCurrent;
+    private Hour[] mHourlyForecast;
+    private Day[] mDailyForecast;
+}
