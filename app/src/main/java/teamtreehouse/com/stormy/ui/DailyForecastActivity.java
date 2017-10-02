a57fc58f960dff68b228d48b31da99ea5b28d01f
Chris Gaona <thebeloved88@sbcglobal.net>
2017-10-02 08:57:31 -0700

    Utilizes a default adapter

--- app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -3,6 +3,7 @@ package teamtreehouse.com.stormy.ui;
 import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import teamtreehouse.com.stormy.R;
 
@@ -15,5 +16,9 @@ public class DailyForecastActivity extends ListActivity {
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                daysOfTheWeek);
+        setListAdapter(adapter);
     }
 }
