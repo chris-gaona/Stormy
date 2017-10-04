c02553aa1c18d392ee2a7b772422f823498b21c9
Chris Gaona <thebeloved88@sbcglobal.net>
2017-10-04 09:36:45 -0700

    Creates custom adapter with ViewHolder pattern; initially wires up to set the data

--- app/src/main/java/teamtreehouse/com/stormy/adapters/DayAdapter.java
+++ app/src/main/java/teamtreehouse/com/stormy/adapters/DayAdapter.java
@@ -1,10 +1,14 @@
 package teamtreehouse.com.stormy.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import teamtreehouse.com.stormy.R;
 import teamtreehouse.com.stormy.weather.Day;
 
 /**
@@ -37,6 +41,37 @@ public class DayAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        ViewHolder holder;
+
+        // check if view is null
+        if (view == null) {
+            // if null it means the view is brand new...need to create everything
+            // inflate a layout
+            view = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) view.findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) view.findViewById(R.id.temperatureLabel);
+            holder.dayLabel = (TextView) view.findViewById(R.id.dayNameLabel);
+
+            // sets tag for view
+            view.setTag(holder);
+        } else {
+            // views are already set up
+            // just need to getTag of view already created
+            holder = (ViewHolder) view.getTag();
+        }
+
+        Day day = mDays[i];
+        holder.iconImageView.setImageResource(day.getIconId());
+        holder.temperatureLabel.setText(day.getTemperatureMax() + "");
+        holder.dayLabel.setText(day.getDayOfTheWeek());
+
+        return view;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView; // public by default
+        TextView temperatureLabel;
+        TextView dayLabel;
     }
 }
