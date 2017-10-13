b46e45a5992dd7842cd90fdc3f021efed638ee2a
Chris Gaona <thebeloved88@sbcglobal.net>
2017-10-13 09:19:20 -0700

    Uses a listview in a regular activity

--- app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -1,28 +1,38 @@
 package teamtreehouse.com.stormy.ui;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Arrays;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import teamtreehouse.com.stormy.R;
 import teamtreehouse.com.stormy.adapters.DayAdapter;
 import teamtreehouse.com.stormy.weather.Day;
 
-public class DailyForecastActivity extends ListActivity {
+public class DailyForecastActivity extends Activity {
     private Day[] mDays;
+
+    @InjectView(android.R.id.list) ListView mListView;
+    @InjectView(android.R.id.empty) TextView mEmptyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
+
+        ButterKnife.inject(this);
 
         Intent intent = getIntent();
         // gets arguments passed from MainActivity
@@ -32,22 +42,22 @@ public class DailyForecastActivity extends ListActivity {
         mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
 
         DayAdapter adapter = new DayAdapter(this, mDays);
-        setListAdapter(adapter);
-    }
+        mListView.setAdapter(adapter);
+        mListView.setEmptyView(mEmptyTextView);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // l = list view where tap occurred
+            // v = view parameter is specific view item that was clicked
+            // position = numerical index of item clicked
+            // long = optional itemID to set
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-    // l = list view where tap occurred
-    // v = view parameter is specific view item that was clicked
-    // position = numerical index of item clicked
-    // long = optional itemID to set
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
-        String conditions = mDays[position].getSummary();
-        String highTemp = mDays[position].getTemperatureMax() + "";
-        String message = String.format("On %s the high will be %s and it will be %s", dayOfTheWeek, highTemp, conditions);
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-
+                String dayOfTheWeek = mDays[i].getDayOfTheWeek();
+                String conditions = mDays[i].getSummary();
+                String highTemp = mDays[i].getTemperatureMax() + "";
+                String message = String.format("On %s the high will be %s and it will be %s", dayOfTheWeek, highTemp, conditions);
+                Toast.makeText(DailyForecastActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
