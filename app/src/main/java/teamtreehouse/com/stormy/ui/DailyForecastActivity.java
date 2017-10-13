95d7c311e48f4ac395f71ac0eff7f7bc215150bd
Chris Gaona <thebeloved88@sbcglobal.net>
2017-10-13 09:09:15 -0700

    Show a toast message when tapping on listview item

--- app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
@@ -30,5 +33,21 @@ public class DailyForecastActivity extends ListActivity {
 
         DayAdapter adapter = new DayAdapter(this, mDays);
         setListAdapter(adapter);
+    }
+
+    // l = list view where tap occurred
+    // v = view parameter is specific view item that was clicked
+    // position = numerical index of item clicked
+    // long = optional itemID to set
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+        String conditions = mDays[position].getSummary();
+        String highTemp = mDays[position].getTemperatureMax() + "";
+        String message = String.format("On %s the high will be %s and it will be %s", dayOfTheWeek, highTemp, conditions);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
     }
 }
